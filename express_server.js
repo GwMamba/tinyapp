be20d9e545746cb0908cc7782b01469ff99981b6
Gerald <gerald.mwang@gmail.com>
2023-10-30 21:01:51 -0600

    We see a JSON string representing the entire urlDatabase object, as it stands at the moment the request is made.

--- express_server.js
+++ express_server.js
@@ -7,9 +7,14 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
