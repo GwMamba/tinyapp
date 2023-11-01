13c98ef98929c013601f3e62e4076f209df3f837
Gerald <gerald.mwang@gmail.com>
2023-11-01 15:36:57 -0600

    used the curl -i to fetch our url.

--- express_server.js
+++ express_server.js
@@ -7,13 +7,19 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 
 app.listen(PORT, () => {
